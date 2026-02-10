bfcd6b85c9ed9cc6db4365579543dd5f477651a6
Isa <isabel.ciartano@gmail.com>
2026-02-10 09:05:25 +0100

    carte di prova

---
+++ codice/carta.js
@@ -0,0 +1,18 @@
+class Carta{
+    constructor(x,y,img){
+        this.x=x;
+        this.y=y;
+        this.imgShow=img;
+
+    }
+
+    flip(imgFronte){
+        this.imgShow=imgFronte;
+    }
+
+
+
+
+
+
+}
