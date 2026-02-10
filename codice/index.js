1bb9837a251c9863d902629ba553de41a61a07a8
Isa <isabel.ciartano@gmail.com>
2026-02-10 09:14:19 +0100

    immgaine e prove iniziali
    prove iniziali sul flip delle carte

--- codice/index.js
+++ codice/index.js
@@ -1,17 +1,27 @@
 let back;
+let carta;
+let imgC;
+let imgF;
 
 function preload() {
   back = loadImage('./img/tavolo.png');
+  imgC=loadImage('./img/carta.png');
+  imgF=loadImage('./img/sette.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60);
+   carta=new Carta(200,200,imgC);
 }
 
 function draw() {
   background(back);
+ 
+  image(carta.imgShow,carta.x,carta.y);
 }
-
+function mouseClicked(){
+  carta.flip(imgF);
+}
 
 
